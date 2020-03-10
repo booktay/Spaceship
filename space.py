0f67ba7c61331760c1577205a569d3e0a64b4474
Siwanont Sittinam <siwanont1@gmail.com>
2020-03-10 23:57:48 +0700

    Add Controller

---
+++ space.py
@@ -0,0 +1,54 @@
+import arcade
+ 
+from models import World, Ship
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+ 
+        super().__init__(*args, **kwargs)
+ 
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+            self.angle = self.model.angle
+ 
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
+class SpaceGameWindow(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+ 
+        arcade.set_background_color(arcade.color.PASTEL_PINK)
+ 
+        self.world = World(width, height) 
+        self.ship_sprite = ModelSprite('images/ship.png',model=self.world.ship)        
+        self.gold_sprite = ModelSprite('images/gold.png',model=self.world.gold)
+
+        self.asteroid_sprites = []
+        for asteroid in self.world.asteroids:
+            self.asteroid_sprites.append(ModelSprite('images/ship.png',scale=0.5,model=asteroid))
+
+    def on_draw(self):
+        arcade.start_render()
+        self.gold_sprite.draw()
+        self.ship_sprite.draw()
+        for sprite in self.asteroid_sprites:
+            sprite.draw()
+        arcade.draw_text(str(self.world.score),self.width - 60, self.height - 30,arcade.color.WHITE, 20)
+ 
+    def animate(self, delta):
+        self.world.animate(delta)
+        #self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y) Have row 28
+
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
+
+if __name__ == '__main__':
+    window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    arcade.run()
